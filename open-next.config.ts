0aff58443fe94e01378c3d77acc521234e730232
crowdmineinvestment1 <crowdmineinvestment1@gmail.com>
2026-08-03 11:24:16 -0700

    fix: simplify open-next config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,1 @@
-import { defineConfig } from "@opennext/manifest";
-import cloudflare from "@opennextjs/cloudflare";
-
-export default defineConfig({
-  adapter: cloudflare(),
-});
+export default {};
